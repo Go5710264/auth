50560b5cad901e049bbccfab896fe6eaae472b57
Dima Shirkov <dimashirkov@list.ru>
2022-08-25 08:07:39 +0300

    dak1 draft v2

--- task.js
+++ task.js
@@ -4,11 +4,6 @@ const userId = document.getElementById('user_id');
 const xhr = new XMLHttpRequest(); 
 const exit = document.getElementById('exit');
 
-// if (localStorage.getItem('user_id')) {
-//     welcomeBlock.classList.add('welcome_active');
-//     userId.textContent = localStorage.getItem('user_id');
-// }
-
 if (document.cookie) {
     const pairs = document.cookie.split('; ');
     const cookie = pairs.find(p => p.startsWith('userId='));
@@ -31,7 +26,6 @@ signinBtn.onclick = function () {
             } else if (key === 'user_id') {
                 userId.textContent = value;
                 document.cookie = `userId=${encodeURIComponent(value)}`;
-                // localStorage.setItem('user_id', value);
 
                 deleteValue();
             } else {
@@ -40,19 +34,17 @@ signinBtn.onclick = function () {
             }
         })
     };
-
-    // стереть данные из полей и добавить деавторизацию
 }
 
 document.forms[0].addEventListener('submit', (e) => {
     e.preventDefault();
 })
 
-function deleteValue () {
+function deleteValue () { // удаление значений из полей ввода
     document.querySelectorAll('.control').forEach((node) => node.value = '')
 }
 
-exit.onclick = function () {
+exit.onclick = function () { // удаление куки
     const pairs = document.cookie.split('; ');
     let cookie = pairs.find(p => p.startsWith('userId='));
     document.cookie = cookie + '; expires=Thu, 01 Jan 1970 00:00:00 GMT'
